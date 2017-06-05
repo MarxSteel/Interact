53554321737f4efbd2f52f0eacd159348bcebc1f
Marx Medeiros <analista.marx@gmail.com>
2017-06-05 01:29:16 -0300

    Dashboard Semi

--- lib/qyuser.php
+++ lib/qyuser.php
@@ -5,6 +5,7 @@
 $db = DB();
 $nickname = $user->username;
 $Distrito = $user->dist;
+$codAss = $user->codAss;
  $query = $db->prepare("SELECT * FROM priv WHERE user='$nickname'");
  $query->execute();
   $row = $query->fetch();
@@ -13,4 +14,25 @@ $Distrito = $user->dist;
   $PriC = $row['priC']; 		// Privilégio de Cadastro de Clubes
   $PriD = $row['priD']; 		// Privilégio de Gerenciar Distrito
   $PriP = $row['priP']; 		// Privilégio de Gerenciar ANP
+
+
+/*
+** CHAMANDO INFORMAÇÕES DO CADASTRO DO ASSOCIADO
+*/
+ $ChamaSocio = $db->prepare("SELECT * FROM ic_socio WHERE id='$codAss'");
+ $ChamaSocio->execute();
+  $Socio = $ChamaSocio->fetch();
+   $nomSocio = $Socio['nomeCom'];		//NOME COMPLETO DO ASSOCIADO
+   $phoSocio = $Socio['foto'];			//FOTO DO ASSOCIADO
+   $CodClub = $Socio['codClub'];		//CODIGO DO CLUBE
+
+
+/*
+** CHAMANDO CLUBE DO ASSOCIADO
+*/
+ $ChamaClube = $db->prepare("SELECT * FROM ic_clube WHERE id='$CodClub'");
+ $ChamaClube->execute();
+  $Cl = $ChamaClube->fetch();
+   $NomeClube = $Cl['clubeNome'];
+
 ?>
---
+++ sidebar.php
@@ -0,0 +1,49 @@
+   <div class="sidebar sidebar-main">
+	<div class="sidebar-content">
+	 <div class="sidebar-user-material">
+	  <div class="category-content">
+	   <div class="sidebar-user-material-content">
+		<a href="#"><img src="assets/images/perfil/<?php echo $phoSocio; ?>" class="img-circle img-responsive" alt=""></a>
+		<h6><?php echo $nomSocio; ?></h6>
+		<span class="text-size-small"><?php echo $NomeClube; ?><br />Distrito <?php echo $Distrito; ?></span>
+	   </div>														
+	   <div class="sidebar-user-material-menu">
+		<a href="#user-nav" data-toggle="collapse"><span>Minha Conta</span> <i class="caret"></i></a>
+	   </div>
+	  </div>
+      <div class="navigation-wrapper collapse" id="user-nav">
+	   <ul class="navigation">
+	    <li><a href="#"><i class="icon-user-plus"></i> <span>Meu Perfil</span></a></li>
+	    <li><a href="#"><i class="icon-coins"></i> <span>Relatório de Uso</span></a></li>
+	    <li class="divider"></li>
+	    <li><a href="#"><i class="icon-cog5"></i> <span>Configura&ccedil;&atilde;o de Perfil</span></a></li>
+	    <li><a href="#"><i class="icon-switch2"></i> <span>Sair do Sistema</span></a></li>
+	   </ul>
+	  </div>
+	 </div>
+	 <div class="sidebar-category sidebar-category-visible">
+	  <div class="category-content no-padding">
+	   <ul class="navigation navigation-main navigation-accordion">
+		<li class="active">
+		 <a href="index.html"><i class="icon-home4"></i> <span>In&iacute;cio</span></a>
+		</li>
+		<li>
+		 <a href="#"><i class="icon-stack2"></i> <span>Page layouts</span></a>
+	      <ul>
+		   <li><a href="layout_navbar_fixed.html">Fixed navbar</a></li>
+		   <li><a href="layout_navbar_sidebar_fixed.html">Fixed navbar &amp; sidebar</a></li>
+		   <li><a href="layout_sidebar_fixed_native.html">Fixed sidebar native scroll</a></li>
+		   <li><a href="layout_navbar_hideable.html">Hideable navbar</a></li>
+		   <li><a href="layout_navbar_hideable_sidebar.html">Hideable &amp; fixed sidebar</a></li>
+		   <li><a href="layout_footer_fixed.html">Fixed footer</a></li>
+		   <li class="navigation-divider"></li>
+		   <li><a href="boxed_default.html">Boxed with default sidebar</a></li>
+		   <li><a href="boxed_mini.html">Boxed with mini sidebar</a></li>
+		   <li><a href="boxed_full.html">Boxed full width</a></li>
+		  </ul>
+		</li>
+	   </ul>
+	  </div>
+	 </div>
+	</div>
+   </div>
