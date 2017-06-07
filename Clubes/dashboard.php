a905598ad4f7c4d5fbb8efd4e469d7f08743cc2f
Marx Medeiros <analista.marx@gmail.com>
2017-06-07 03:20:34 -0300

    Micro Revisão

--- Clubes/dashboard.php
+++ Clubes/dashboard.php
@@ -2,12 +2,9 @@
 include_once '../sess.php';
 include_once '../dados.php';
 include_once '../lib/qyuser.php';
-$cHome = 'class="active"';
-
-
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt">
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -31,14 +28,28 @@ $cHome = 'class="active"';
 	<!-- /core JS files -->
 
 	<!-- Theme JS files -->
-	<script type="text/javascript" src="../assets/js/plugins/tables/datatables/datatables.min.js"></script>
+ <script type="text/javascript" src="../assets/js/plugins/tables/datatables/datatables.min.js"></script>
+ <script type="text/javascript" src="../assets/js/pages/datatables_basic.js"></script>
+
+	<!-- Theme JS files -->
+	<script type="text/javascript" src="../assets/js/core/libraries/jquery_ui/core.min.js"></script>
+	<script type="text/javascript" src="../assets/js/plugins/forms/wizards/form_wizard/form.min.js"></script>
+	<script type="text/javascript" src="../assets/js/plugins/forms/wizards/form_wizard/form_wizard.min.js"></script>
 	<script type="text/javascript" src="../assets/js/plugins/forms/selects/select2.min.js"></script>
+	<script type="text/javascript" src="../assets/js/plugins/forms/styling/uniform.min.js"></script>
+	<script type="text/javascript" src="../assets/js/core/libraries/jasny_bootstrap.min.js"></script>
+	<script type="text/javascript" src="../assets/js/plugins/forms/validation/validate.min.js"></script>
+	<script type="text/javascript" src="../assets/js/plugins/notifications/sweet_alert.min.js"></script>
 
 	<script type="text/javascript" src="../assets/js/core/app.js"></script>
-	<script type="text/javascript" src="../assets/js/pages/datatables_basic.js"></script>
+	<script type="text/javascript" src="../assets/js/pages/wizard_form.js"></script>
 
 	<script type="text/javascript" src="../assets/js/plugins/ui/ripple.min.js"></script>
 	<!-- /theme JS files -->
+
+
+
+
 
 </head>
 
@@ -102,9 +113,7 @@ $cHome = 'class="active"';
 	  </div>
 	  <div class="heading-elements">
 	   <div class="heading-btn-group">
-		<a href="../#" class="btn btn-link btn-float text-size-small has-text"><i class="icon-bars-alt text-primary"></i><span>Estat&iacute;sticas</span></a>
-		<a href="../#" class="btn btn-link btn-float text-size-small has-text"><i class="icon-calculator text-primary"></i> <span>Cadastrar Clube</span></a>
-		<a href="../#" class="btn btn-link btn-float text-size-small has-text"><i class="icon-calendar5 text-primary"></i> <span>Manuais</span></a>
+		<button type="button" class="btn bg-blue-400 btn-sm btn-labeled btn-rounded" data-toggle="modal" data-target="#modal_default"><b><i class="icon-play3 position-right"></i></b> Cadastrar Club</button>
 	   </div>
 	  </div>
 	 </div>
@@ -216,12 +225,191 @@ $cHome = 'class="active"';
 	 </div>
 	</div>
 
+<div id="modal_default" class="modal fade">
+ <div class="modal-dialog modal-lg">
+  <div class="modal-content">
+   <div class="modal-header">
+	<button type="button" class="close" data-dismiss="modal">&times;</button>
+	<h5 class="modal-title">Basic modal</h5>
+   </div>
+   <div class="modal-body">
 
-<div id="myModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-    <div class="modal-content">
+
+<form class="form-ajax" action="ajax.php" method="post">
+ <fieldset class="step" id="ajax-step1">
+  <h6 class="form-wizard-title text-semibold">
+   <span class="form-wizard-count">1</span>Informações do Clube
+  </h6>
+  <div class="row">
+   <div class="col-md-6">
+	<div class="form-group">
+	 <label>Interact Club de:</label>
+	 <input type="text" name="clubeNome" class="form-control" placeholder="John Doe">
     </div>
+   </div>
+   <div class="col-md-6">
+   <label>Date of birth:</label>
+   <input type="text" name="DataFund" class="form-control" minlength="10" maxlength="10" OnKeyPress="formatar('##/##/####', this)" required="required" placeholder="Data">
+   </div>
+   <div class="col-md-12">
+	<div class="form-group">
+	 <label>Rotary Club(s) Patrocinador(es):</label>
+	 <input type="text" name="clubeNome" class="form-control" placeholder="John Doe">
+    </div>
+   </div>
   </div>
+ </fieldset>
+ <fieldset class="step" id="ajax-step2">
+  <h6 class="form-wizard-title text-semibold">
+   <span class="form-wizard-count">2</span>
+	Informações de Endereço e de Reunião
+	<small class="display-block"></small>
+  </h6>
+  <div class="row">
+   <div class="col-md-8">
+	<div class="form-group">
+     <label>Locald e Reunião:</label>
+      <input type="text" name="local" placeholder="Ex.: Casa da Amizade" class="form-control">
+	</div>
+   </div>
+   <div class="col-md-4">
+	<div class="form-group">
+     <label>Horário:</label>
+      <input type="text" name="horario" placeholder="22:00" minlength="5" maxlength="5" class="form-control">
+	</div>
+   </div>
+   <div class="col-md-6">
+	<div class="form-group">
+	 <label>Periodo:</label>
+     <select name="periodo" data-placeholder="Selecione um Estado..." class="select">
+	  <option></option> 
+      <option value="Semanal"> Semanal</option>
+      <option value="Quinzenal"> Quinzenal</option>
+      <option value="Mensal"> Mensal</option>
+	 </select>
+	</div>
+  </div>
+   <div class="col-md-6">
+	<div class="form-group">
+	 <label>Dia da Semana:</label>
+     <select name="semana" data-placeholder="Selecione um Estado..." class="select">
+	  <option></option> 
+      <option value="SEG"> Segunda-Feira</option>
+      <option value="TER"> Terça-Feira</option>
+      <option value="Qua"> Quarta-Feira</option>
+      <option value="Qui"> Quinta Feira</option>
+      <option value="SEX"> Sexta-Feira</option>
+      <option value="SAB"> S&aacute;bado</option>
+      <option value="DOM"> Domingo</option>
+	 </select>
+	</div>
+  </div>
+   <div class="col-md-8">
+	<div class="form-group">
+     <label>Rua:</label>
+      <input type="text" name="rua" placeholder="Ex.: Avenida 7 de Setembro" class="form-control">
+	</div>
+   </div>
+   <div class="col-md-4">
+	<div class="form-group">
+     <label>Número:</label>
+      <input type="text" name="rua" placeholder="1234" class="form-control">
+	</div>
+   </div>
+   <div class="col-md-4">
+	<div class="form-group">
+     <label>CEP</label>
+      <input type="text" name="cep" class="form-control">
+	</div>
+   </div>
+   <div class="col-md-8">
+	<div class="form-group">
+     <label>Bairro/setor:</label>
+      <input type="text" name="bairro" class="form-control">
+	</div>
+   </div>
+   <div class="col-md-6">
+	<div class="form-group">
+     <label>Cidade</label>
+      <input type="text" name="cidade" class="form-control">
+	</div>
+   </div>
+   <div class="col-md-6">
+	<div class="form-group">
+	 <label>Estado:</label>
+     <select name="UF" data-placeholder="Selecione um Estado..." class="select">
+	  <option></option> 
+         <option value="AC"> Acre</option>
+         <option value="AL"> Alagoas</option>
+         <option value="AM"> Amap&aacute;</option>
+         <option value="BA"> Bahia</option>
+         <option value="CE"> Cear&aacute;</option>
+         <option value="DF"> Distrito Federal</option>
+         <option value="ES"> Esp&iacute;rito Santo</option>
+         <option value="GO"> Goi&aacute;</option>
+         <option value="MA"> Maranh&atilde;o</option>
+         <option value="MT"> Mato Grosso</option>
+         <option value="MS"> Mato Grosso do Sul</option>
+         <option value="MG"> Minas Gerais</option>
+         <option value="PA"> Par&aacute;</option>
+         <option value="PB"> Para&iacute;ba</option>
+         <option value="PR"> Paran&aacute;</option>
+         <option value="PE"> Pernambuco</option>
+         <option value="PI"> Piau&iacute;</option>
+         <option value="RJ"> Rio de Janeiro</option>
+         <option value="RN"> Rio Grande do Norte</option>
+         <option value="RS"> Rio Grande do Sul</option>
+         <option value="RO"> Rond&ocirc;nia</option>
+         <option value="RR"> Roraima</option>
+         <option value="SC"> Santa Catarina</option>
+         <option value="SP"> S&atilde;o Paulo</option>
+         <option value="SE"> Sergipe</option>
+         <option value="TO"> Tocantins</option>
+	 </select>
+	</div>
+  </div>
+ </div>
+ </fieldset>
+ <fieldset class="step" id="ajax-step3">
+  <h6 class="form-wizard-title text-semibold">
+   <span class="form-wizard-count">3</span>
+	Contato e Redes Sociais
+  </h6>
+  <div class="row">
+   <div class="col-md-6">
+    <div class="form-group">
+	 <label>E-Mail do Clube</label>
+	  <input type="email" name="mail1" class="form-control" placeholder="your@email.com">
+	</div>
+   </div>
+  <div class="col-md-6">
+    <div class="form-group">
+	 <label>E-Mail para Projetos</label>
+	  <input type="email" name="mail2" class="form-control" placeholder="your@email.com">
+	</div>
+   </div>
+  <div class="col-md-6">
+   <div class="form-group">
+	<label>Telefone 1</label>
+	 <input type="text" name="tel1" class="form-control" placeholder="66-777-888-999" data-mask="99-999-999-999">
+	</div> 
+  </div>
+  <div class="col-md-6">
+   <div class="form-group">
+	<label>Telefone 2</label>
+	 <input type="text" name="tel2" class="form-control" placeholder="66-777-888-999" data-mask="99-999-999-999">
+	</div> 
+  </div>
+ </div>
+</fieldset>
+<div class="form-wizard-actions">
+ <button class="btn btn-default" id="ajax-back" type="reset">Voltar</button>
+ <button class="btn btn-info" id="ajax-next" type="submit">Proximo</button>
+</div>
+</form>
+ </div>
+  </div>
+ </div>
 </div>
 
 				<!-- /content area -->
@@ -235,22 +423,6 @@ $cHome = 'class="active"';
 	</div>
 	<!-- /page container -->
 
-<script>
-    $('.ModalInativa').click(function(){
-        var id = $(this).attr('data-id');
-        $.ajax({url:"Inativar.php?id="+id,cache:false,success:function(result){
-            $(".modal-content").html(result);
-        }});
-    });
-</script>
-<script>
-    $('.ModalAtiva').click(function(){
-        var id = $(this).attr('data-id');
-        $.ajax({url:"Ativar.php?id="+id,cache:false,success:function(result){
-            $(".modal-content").html(result);
-        }});
-    });
-</script>
 <script language="JavaScript">
 function abrir(URL) {
  
@@ -264,7 +436,18 @@ function abrir(URL) {
  
 }
 </script>
-
+<script>
+function formatar(mascara, documento){
+  var i = documento.value.length;
+  var saida = mascara.substring(0,1);
+  var texto = mascara.substring(i)
+  
+  if (texto.substring(0,1) != saida){
+            documento.value += texto.substring(0,1);
+  }
+  
+}
+</script>
 
 </body>
 </html>
